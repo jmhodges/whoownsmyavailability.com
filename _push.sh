7c5f0985d77cd8bfca051d0ca2bdf266b68587c6
Jeff Hodges <jeff@somethingsimilar.com>
2011-04-23 15:52:57 -0700

    changing release directory name

--- _push.sh
+++ _push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-stamp=`date -u +%Y%m%d%H%M%S`
-release="whoownsmyavailability.com-$stamp"
+release=`date -u +%Y%m%d%H%M%S`
 release_dir="whoownsmyavailability.com_releases"
 path=$release_dir/$release
 relink_cmd="rm ~/whoownsmyavailability.com && ln -s ~/$path ~/whoownsmyavailability.com"
