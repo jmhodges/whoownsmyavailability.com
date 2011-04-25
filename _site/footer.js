d0b85ebc7f68a3284f24979603e36c1bfac2106a
tnm <tnm800@gmail.com>
2011-04-25 00:39:13 -0700

    add authors, increase font size

--- _site/footer.js
+++ _site/footer.js
@@ -1,19 +1,19 @@
 var links = [
   {
    "href": "http://blog.b3k.us/2009/07/15/service-level-disagreements.html",
-   "text": "Service Level Disagreements"
+   "text": "Service Level Disagreements, by Benjamin Black"
    },
   {
     "href": "http://blog.b3k.us/2009/07/16/service-level-disagreements-2.html",
-    "text": "Service Level Disagreements, Part 2"
+    "text": "Service Level Disagreements, by Benjamin Black"
   },
   {
     "href": "http://www.kitchensoap.com/2011/04/07/resilience-engineering-part-i/",
-    "text": "Resilience Engineering: Part I"
+    "text": "Resilience Engineering: Part I, by John Allspaw"
   },
   {
     "href": "http://www.kitchensoap.com/2009/11/12/how-complex-systems-fail-a-webops-perspective/",
-    "text": "How Complex Systems Fail: A WebOps Perspective"
+    "text": "How Complex Systems Fail: A WebOps Perspective, by John Allspaw"
   }
 ];
 var footer = document.getElementById("footer");
