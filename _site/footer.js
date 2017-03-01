785393bd68f85d83e020a3f52f855bf566026385
Jeff Hodges <jeff@somethingsimilar.com>
2017-03-01 14:39:29 -0800

    remove some posts and adjust maintainer list

--- _site/footer.js
+++ _site/footer.js
@@ -1,13 +1,5 @@
 var links = [
   {
-   "href": "http://blog.b3k.us/2009/07/15/service-level-disagreements.html",
-   "text": "Service Level Disagreements, by Benjamin Black"
-   },
-  {
-    "href": "http://blog.b3k.us/2009/07/16/service-level-disagreements-2.html",
-    "text": "Service Level Disagreements, Part 2, by Benjamin Black"
-  },
-  {
     "href": "http://www.kitchensoap.com/2011/04/07/resilience-engineering-part-i/",
     "text": "Resilience Engineering: Part I, by John Allspaw"
   },
